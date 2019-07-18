7024db412ac8619ad06585d2a730aa3010e3cabd
Amol Jagtap <amoljagtap17@gmail.com>
2019-07-19 01:21:47 +0530

    NEXTJS-STYLED-COMPONENTS-BEACHWALK-RESORT: Add Rooms component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import GlobalStyles from '../components/globals/GlobalStyles'
 import Header from '../components/home/Header'
 import About from '../components/home/About'
+import Rooms from '../components/home/Rooms'
 
 const Home = () => (
   <>
     <GlobalStyles />
     <Header />
     <About />
+    <Rooms />
   </>
 )
 
---
+++ utils/rooms-data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    img: 'room1.jpeg',
+    title: 'basic economy',
+    info:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quisquam corrupti dicta tempora sequi sapiente!',
+    price: 125
+  },
+  {
+    id: 2,
+    img: 'room2.jpeg',
+    title: 'presidential suite',
+    info:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quisquam corrupti dicta tempora sequi sapiente!',
+    price: 250
+  },
+  {
+    id: 3,
+    img: 'room3.jpeg',
+    title: 'standard queen room',
+    info:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est quisquam corrupti dicta tempora sequi sapiente!',
+    price: 375
+  }
+]
