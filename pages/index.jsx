11d4c595b7f0357fd3956dd0965af337d10cdab4
Amol Jagtap <amoljagtap17@gmail.com>
2019-07-19 01:08:52 +0530

    NEXTJS-STYLED-COMPONENTS-BEACHWALK-RESORT: Add About component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import GlobalStyles from '../components/globals/GlobalStyles'
 import Header from '../components/home/Header'
+import About from '../components/home/About'
 
 const Home = () => (
   <>
     <GlobalStyles />
     <Header />
+    <About />
   </>
 )
 
